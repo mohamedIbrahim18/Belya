7d05e8da41159e2566e42ee26fb65abbac34227a
MOHAMEDIBRAHIM-\Mohamed Ibrahim <mohamedibrahemofficial@gmail.com>
2024-01-09 05:31:27 +0200

    +init app

---
+++ app/src/main/java/com/example/belya/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.belya.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.belya.R
+import com.example.belya.databinding.ActivitySplashScreenBinding
+import com.example.belya.ui.signup.SignUpActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    lateinit var viewBinding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                goToSignUpActivity()
+            },1000)
+    }
+
+    private fun goToSignUpActivity() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}

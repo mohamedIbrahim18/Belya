3b243ff20707773bb43bf3d08dc5ff9573bef503
MOHAMEDIBRAHIM-\Mohamed Ibrahim <mohamedibrahemofficial@gmail.com>
2024-01-12 22:12:14 +0200

    commit

--- app/src/main/java/com/example/belya/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/belya/ui/splashscreen/SplashScreenActivity.kt
@@ -1,30 +1,28 @@
 package com.example.belya.ui.splashscreen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.example.belya.R
 import com.example.belya.databinding.ActivitySplashScreenBinding
 import com.example.belya.ui.signup.SignUpActivity
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity: AppCompatActivity() {
     lateinit var viewBinding : ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        viewBinding= ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        Handler(Looper.getMainLooper())
-            .postDelayed({
-                goToSignUpActivity()
-            },1000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            gotoSignUp()
+        },1000)
     }
 
-    private fun goToSignUpActivity() {
-        val intent = Intent(this, SignUpActivity::class.java)
+    private fun gotoSignUp() {
+        val intent = Intent(this,SignUpActivity::class.java)
         startActivity(intent)
-        finish()
     }
 }

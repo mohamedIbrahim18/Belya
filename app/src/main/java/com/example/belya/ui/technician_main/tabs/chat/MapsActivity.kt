c3f2760b450dd0a8c0b950eff0d0386dc454bf70
MOHAMEDIBRAHIM-\Mohamed Ibrahim <mohamedibrahemofficial@gmail.com>
2024-04-22 10:37:21 +0200

    / change size zoom in maps

--- app/src/main/java/com/example/belya/ui/technician_main/tabs/chat/MapsActivity.kt
+++ app/src/main/java/com/example/belya/ui/technician_main/tabs/chat/MapsActivity.kt
@@ -200,7 +200,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     } else {
                         userMarker?.position = latLng
                     }
-                    googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13.0f))
+                    googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f))
                 } else {
                     Log.e("User location", "Document does not exist for user: $userId")
                 }
